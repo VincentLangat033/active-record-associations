c602fad97956cc05fddde56590b3aa909d10dc32
Vincent Langat <kimutailangat033@gmail.com>
2022-09-07 12:20:56 +0300

    input signup validations

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,56 @@
+class AuthorsController < ApplicationController
+
+
+    # responsible for anything involvingour author
+
+
+    # sign up  '/signup'
+    # new => creating an object
+
+    get '/signup' do 
+        erb :"authors/signup"
+
+    end
+    post '/signup' do 
+        # binding.pry
+
+
+        # process signup form
+        # receive data from the form inside our params hash
+        # create a new author object with the data
+
+        author = Author.new(params)
+
+        # persist the object
+        # make sure user is signing up with unique data
+        # validate our author object
+
+        # if author.username != ""
+        # if author.username.empty?
+        if author.username.blank? || author.email.blank? || author.name.blank? || author.password.blank? || Author.find_by_email(params[:email]) || Author.find_by_username(params[:username])
+            # redirect them back to signup page
+
+            redirect '/signup'
+        else
+            author.save
+            redirect '/posts'
+
+    
+    
+        # else
+            # if our user is valid
+            #  persist the new object
+            # redirect them somewher
+
+        end
+            
+
+
+    end
+
+    # login  '/login'
+    # read action => querying author and reading author atribbutes
+
+    # logout
+    # delete action => since it clears the sessions
+end
--- config.ru
+++ config.ru
@@ -3,5 +3,7 @@ require './config/environment'
 
 use Rack::MethodOverride
 use PostsControllers
+use AuthorsController
 run ApplicationController 
+
 # use controller_name
